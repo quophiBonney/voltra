0978070431010812d87f4f77ab797fdceb4f840f
quophiBonney <quophiclef@gmail.com>
2026-05-25 23:43:36 +0000

    working on the onboarding

--- src/app/(onboarding)/index.tsx
+++ src/app/(onboarding)/index.tsx
@@ -1,31 +1,50 @@
-import React from "react";
 import { Image } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
-const OnboardingScreen: React.FC = () => {
-  const pages = [
-    {
-      backgroundColor: "#a6e4d0",
-      image: React.createElement(Image, {
-        source: require("../../../assets/images/bulb.png"),
-        style: { width: 200, height: 200 },
-      }),
-      title: "Know Your Energy Consumption",
-      subtitle:
-        "An easy and seamless way to know the energy consumption of your appliances",
-    },
-    {
-      backgroundColor: "#fdeb93",
-      image: React.createElement(Image, {
-        source: require("../../../assets/images/document.png"),
-        style: { width: 200, height: 200 },
-      }),
-      title: "Read Documents By Listening",
-      subtitle: "Listen to your documents instead Of reading them On the goal",
-    },
-  ];
+const OnboardingScreen = () => {
+  const insets = useSafeAreaInsets();
 
-  return React.createElement(Onboarding, { pages });
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
+      <Onboarding
+        bottomBarHeight={90 + insets.bottom}
+        onSkip={() => console.log("Skipped")}
+        onDone={() => console.log("Done")}
+        pages={[
+          {
+            backgroundColor: "#a6e4d0",
+            image: (
+              <Image
+                source={require("../../../assets/images/bulb.png")}
+                style={{ width: 200, height: 200 }}
+                resizeMode="contain"
+              />
+            ),
+            title: "Know Your Energy Consumption",
+            subtitle:
+              "An easy and seamless way to know the energy consumption of your appliances",
+          },
+          {
+            backgroundColor: "#fdeb93",
+            image: (
+              <Image
+                source={require("../../../assets/images/document.png")}
+                style={{ width: 200, height: 200 }}
+                resizeMode="contain"
+              />
+            ),
+            title: "Read Documents By Listening",
+            subtitle:
+              "Listen to your documents instead of reading them on the go",
+          },
+        ]}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default OnboardingScreen;
