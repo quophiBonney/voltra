0f08f9a41b96e1cd9e296b2f814c33e6db098ad3
quophiBonney <quophiclef@gmail.com>
2026-05-26 12:01:19 +0000

    commit

--- src/app/(onboarding)/index.tsx
+++ src/app/(onboarding)/index.tsx
@@ -1,22 +1,24 @@
-import { Image } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const OnboardingScreen = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
+    <View
+      style={[
+        styles.container,
+        { paddingLeft: insets.left, paddingRight: insets.right },
+      ]}
+    >
       <Onboarding
         bottomBarHeight={90 + insets.bottom}
         onSkip={() => console.log("Skipped")}
         onDone={() => console.log("Done")}
         pages={[
           {
-            backgroundColor: "#a6e4d0",
+            backgroundColor: "#336DF6",
             image: (
               <Image
                 source={require("../../../assets/images/bulb.png")}
@@ -29,7 +31,7 @@ const OnboardingScreen = () => {
               "An easy and seamless way to know the energy consumption of your appliances",
           },
           {
-            backgroundColor: "#fdeb93",
+            backgroundColor: "#336DF6",
             image: (
               <Image
                 source={require("../../../assets/images/document.png")}
@@ -39,12 +41,18 @@ const OnboardingScreen = () => {
             ),
             title: "Read Documents By Listening",
             subtitle:
-              "Listen to your documents instead of reading them on the go",
+              "Listen to your documents instead of reading them on the go.",
           },
         ]}
       />
-    </SafeAreaView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default OnboardingScreen;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,34 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { DefaultTheme, ThemeProvider } from "expo-router/react-navigation";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts("./assets/fonts/Poppins-Black.otf");
+
+  useEffect(() => {
+    if (loaded && error) {
+    }
+  }, [loaded, error]);
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen
-          name="(onboarding)/index"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={DefaultTheme}>
+        <Stack>
+          <Stack.Screen
+            name="(onboarding)/index"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
